79fa73127d3c7b8df09bfc71768940f1a539ed3b
Zuzeka Zimba <ZZimba@jhb.dvt.co.za>
2018-06-16 22:55:16 +0200

    Add project files.

---
+++ MusicLabelCompany/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MusicLabelCompany.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
